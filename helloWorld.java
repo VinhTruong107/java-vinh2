d1758e972af5d9c107d47bc52ca7b13eae0d0ebc
vdq <vietdq19042005@gmail.com>
2024-10-09 21:57:36 +0700

    HelloWorld

--- helloWorld.java
+++ helloWorld.java
@@ -2,5 +2,6 @@
 public class helloWorld {
      public static void main(String[]args){
           System.out.println("Neu no chay duoc thi may ong thay file nay");
+          System.out.println("Test123");
      }     
 }
